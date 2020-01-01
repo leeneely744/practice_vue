1beed5d7e0432873d5a77c0c593ce5c195741302
green bag <nlee9337@gmail.com>
2020-01-01 18:08:37 +0900

    first commit

---
+++ template.vue
@@ -0,0 +1,10 @@
+<script src="https://unpkg.com/vue@2.5.17"></script>
+<script src="https://unpkg.com/vue-router@3.0.1"></script>
+
+<div id="app">
+  <router-link to="/top">トップページ</router-link>
+  <router-link to="/users">ユーザー一覧ページ</router-link>
+  <router-view></router-view>
+</div>
+
+
