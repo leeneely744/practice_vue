a16411fcb70fc9bee939ca3d968949628d1cf75b
green bag <nlee9337@gmail.com>
2020-01-03 18:14:33 +0900

    install pug

--- 6/other-lang/other-lang/src/App.vue
+++ 6/other-lang/other-lang/src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    hello-world(msg="Welcome to Your Vue.js + TypeScript App")
 </template>
 
 <script lang="ts">
